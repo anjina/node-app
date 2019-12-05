c7d146f53b7a550385d91c8afac879c4a5c17582
ANJINA <947937624@qq.com>
2019-12-06 00:25:10 +0800

    feat: init label

---
+++ src/controller/label.js
@@ -0,0 +1,12 @@
+const Base = require('./base.js');
+
+module.exports = class extends Base {
+  async getAction() {
+  }
+
+  async postAction() {}
+
+  async putAction() {}
+
+  async deleteAction() {}
+};
